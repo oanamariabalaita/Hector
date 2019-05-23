9af3fd80978f232cce76e8acbdc6074a01228cf4
obalaita <oana4hi@gmail.com>
2019-05-23 17:55:12 +0300

    popup menu syle

--- app/src/main/java/com/example/hector/ui/main/dashboard/view/DashboardFragment.kt
+++ app/src/main/java/com/example/hector/ui/main/dashboard/view/DashboardFragment.kt
@@ -11,10 +11,10 @@ import android.util.Log
 import android.view.*
 import android.widget.Toast
 import androidx.appcompat.widget.PopupMenu
+import androidx.core.view.setPadding
 import com.example.hector.R
 import com.example.hector.design.RadarMarkerView
 import com.github.mikephil.charting.animation.Easing
-import com.github.mikephil.charting.components.Legend
 import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.components.YAxis
 import kotlinx.android.synthetic.main.item_layout_summary.*
@@ -23,53 +23,16 @@ import com.github.mikephil.charting.interfaces.datasets.IRadarDataSet
 import com.github.mikephil.charting.data.RadarDataSet
 import com.github.mikephil.charting.data.RadarEntry
 import com.github.mikephil.charting.formatter.ValueFormatter
+import java.lang.reflect.Field
+import java.util.*
+import kotlin.collections.ArrayList
+import android.R.attr.popupMenuStyle
+
+
 
 
 @SuppressWarnings("MagicNumber", "LongMethod")
 class DashboardFragment : BaseFragment(), View.OnClickListener, PopupMenu.OnMenuItemClickListener {
-
-    override fun onClick(v: View?) {
-        when (v?.id) {
-            R.id.btn_menu -> {
-                Toast.makeText(context, "HAHAHA", Toast.LENGTH_SHORT).show()
-
-                val popup = PopupMenu(context!!, btn_menu)
-                val inflater: MenuInflater = popup.menuInflater
-                inflater.inflate(R.menu.header_menu_dash, popup.menu)
-                popup.setOnMenuItemClickListener(this@DashboardFragment)
-                popup.show()
-            }
-
-        }
-    }
-
-
-    override fun onMenuItemClick(item: MenuItem?): Boolean {
-        return when (item?.itemId) {
-            R.id.action_add_card -> {
-                Toast.makeText(context, "1", Toast.LENGTH_SHORT).show()
-                true
-            }
-            R.id.action_custom_score -> {
-                Toast.makeText(context, "2", Toast.LENGTH_SHORT).show()
-                true
-            }
-            R.id.action_edit_dash -> {
-                Toast.makeText(context, "3", Toast.LENGTH_SHORT).show()
-                true
-            }
-            R.id.action_save_gallery -> {
-                Toast.makeText(context, "4", Toast.LENGTH_SHORT).show()
-                true
-            }
-            R.id.action_reset_val -> {
-                Toast.makeText(context, "5", Toast.LENGTH_SHORT).show()
-                true
-            }
-
-            else -> false
-        }
-    }
 
     companion object {
 
@@ -87,7 +50,8 @@ class DashboardFragment : BaseFragment(), View.OnClickListener, PopupMenu.OnMenu
     lateinit var tfLight: Typeface
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_dashboard, container, false)
@@ -96,40 +60,39 @@ class DashboardFragment : BaseFragment(), View.OnClickListener, PopupMenu.OnMenu
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         //   presenter.onAttach(this)
         super.onViewCreated(view, savedInstanceState)
-        initData()
         initViews()
         initSummaryChart()
+        initData()
         heathCardAdapter.refreshList(healthCardList)
     }
 
     private fun initSummaryChart() {
         context?.let { ctx ->
 
-            chart_summary.description.setEnabled(false)
+            chart_summary.description.isEnabled = false
+            chart_summary.legend.isEnabled = false
             chart_summary.webLineWidth = 1f
-            chart_summary.webColor = Color.GRAY
+            chart_summary.webColor = Color.BLUE
             chart_summary.webLineWidthInner = 1f
             chart_summary.webColorInner = Color.LTGRAY
             chart_summary.webAlpha = 100
 
-            val mv = RadarMarkerView(ctx, R.layout.radar_markerview)
-            mv.chartView = chart_summary
-            chart_summary.marker = mv
+//            val mv = RadarMarkerView(ctx, R.layout.radar_markerview)
+//            mv.chartView = chart_summary
+//            chart_summary.marker = mv
 
             setData()
-
             chart_summary.animateXY(1400, 1400, Easing.EaseInOutQuad)
             tfLight = Typeface.createFromAsset(ctx.assets, "OpenSans-Light.ttf")
-
             val xAxis: XAxis = chart_summary.xAxis
             xAxis.typeface = tfLight
-            xAxis.textSize = 9f
+            xAxis.textSize = 10f
             xAxis.yOffset = 0f
             xAxis.xOffset = 0f
             xAxis.valueFormatter = object : ValueFormatter() {
 
                 var mActivities: Array<String> =
-                    arrayOf("Weight Management", "Nutrition", "Exercise", "Stress", "Sleep", "Social Life")
+                    arrayOf("Weight", "Food", "Sport", "Sun", "Sleep", "Mood+")
 
                 override fun getFormattedValue(value: Float): String {
                     return mActivities[(value % mActivities.size).toInt()]
@@ -139,22 +102,13 @@ class DashboardFragment : BaseFragment(), View.OnClickListener, PopupMenu.OnMenu
             xAxis.textColor = Color.BLACK
 
             val yAxis: YAxis = chart_summary.yAxis
-            yAxis.setTypeface(tfLight)
+            yAxis.typeface = tfLight
             yAxis.setLabelCount(6, false)
-            yAxis.setTextSize(9f)
-            yAxis.setAxisMinimum(0f)
-            yAxis.setAxisMaximum(80f)
+            yAxis.textSize = 10f
+            yAxis.axisMinimum = 0f
+            yAxis.axisMaximum = 40f
             yAxis.setDrawLabels(false)
 
-            val l: Legend = chart_summary.legend
-            l.verticalAlignment = Legend.LegendVerticalAlignment.BOTTOM
-            l.horizontalAlignment = Legend.LegendHorizontalAlignment.CENTER
-            l.orientation = Legend.LegendOrientation.HORIZONTAL
-            l.setDrawInside(false)
-            l.setTypeface(tfLight)
-            l.setXEntrySpace(7f)
-            l.setYEntrySpace(5f)
-            l.setTextColor(Color.BLACK)
 
         }
     }
@@ -267,15 +221,13 @@ class DashboardFragment : BaseFragment(), View.OnClickListener, PopupMenu.OnMenu
 
     private fun setData() {
 
-        val mul = 80f
-        val min = 20f
-        val cnt = 5
+        val mul = 30f
+        val min = 10f
+        val cnt = 6
 
         val entries1 = ArrayList<RadarEntry>()
         val entries2 = ArrayList<RadarEntry>()
 
-        // NOTE: The order of the entries when being added to the entries array determines their position around the center of
-        // the chart.
         for (i in 0 until cnt) {
             val val1 = (Math.random() * mul).toFloat() + min
             entries1.add(RadarEntry(val1))
@@ -288,8 +240,8 @@ class DashboardFragment : BaseFragment(), View.OnClickListener, PopupMenu.OnMenu
         set1.color = Color.rgb(103, 110, 129)
         set1.fillColor = Color.rgb(103, 110, 129)
         set1.setDrawFilled(true)
-        set1.fillAlpha = 180
-        set1.lineWidth = 2f
+        //   set1.fillAlpha = 180
+        set1.lineWidth = 1f
         set1.isDrawHighlightCircleEnabled = true
         set1.setDrawHighlightIndicators(false)
 
@@ -297,8 +249,8 @@ class DashboardFragment : BaseFragment(), View.OnClickListener, PopupMenu.OnMenu
         set2.color = Color.rgb(121, 162, 175)
         set2.fillColor = Color.rgb(121, 162, 175)
         set2.setDrawFilled(true)
-        set2.fillAlpha = 180
-        set2.lineWidth = 2f
+        //    set2.fillAlpha = 180
+        set2.lineWidth = 1f
         set2.isDrawHighlightCircleEnabled = true
         set2.setDrawHighlightIndicators(false)
 
@@ -308,13 +260,67 @@ class DashboardFragment : BaseFragment(), View.OnClickListener, PopupMenu.OnMenu
         sets.add(set2)
 
         val data = RadarData(sets)
-        data.setValueTypeface(tfLight)
+        //  data.setValueTypeface(tfLight)
         data.setValueTextSize(8f)
         data.setDrawValues(false)
-        data.setValueTextColor(Color.WHITE)
+        data.setValueTextColor(Color.BLACK)
 
         chart_summary.data = data
         chart_summary.invalidate()
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btn_menu -> {
+                val wrapper = ContextThemeWrapper(context, R.style.popupMenuStyle)
+                val popup = PopupMenu(wrapper, btn_menu)
+                val inflater: MenuInflater = popup.menuInflater
+                inflater.inflate(R.menu.header_menu_dash, popup.menu)
+
+                try {
+                    val fieldMPopup = PopupMenu::class.java.getDeclaredField("mPopup")
+                    fieldMPopup.isAccessible = true
+                    val mPopup = fieldMPopup.get(popup)
+                    mPopup.javaClass
+                        .getDeclaredMethod("setForceShowIcon", Boolean::class.java)
+                        .invoke(mPopup, true)
+                } catch (e: Exception) {
+                    Log.e("Main", "Error showing menu icons.", e)
+                } finally {
+                    popup.setOnMenuItemClickListener(this@DashboardFragment)
+                    popup.show()
+                }
+
+                //             popup.show()
+            }
+        }
+    }
+
+    override fun onMenuItemClick(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            R.id.action_add_card -> {
+                Toast.makeText(context, "1", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.action_custom_score -> {
+                Toast.makeText(context, "2", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.action_edit_dash -> {
+                Toast.makeText(context, "3", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.action_save_gallery -> {
+                Toast.makeText(context, "4", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.action_reset_val -> {
+                Toast.makeText(context, "5", Toast.LENGTH_SHORT).show()
+                true
+            }
+
+            else -> false
+        }
     }
 
     override fun setUp() {
