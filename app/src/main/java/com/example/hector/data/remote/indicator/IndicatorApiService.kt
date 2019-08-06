e1419c2731fec757637942e05d52108eedcde873
obalaita <oana4hi@gmail.com>
2019-08-06 23:57:01 +0200

    remote health indicator data implemented

--- app/src/main/java/com/example/hector/data/remote/indicator/IndicatorApiService.kt
+++ app/src/main/java/com/example/hector/data/remote/indicator/IndicatorApiService.kt
@@ -1,3 +1,15 @@
 package com.example.hector.data.remote.indicator
 
-class IndicatorApiService
+import com.example.hector.data.local.indicator.HealthIndicator
+import io.reactivex.Observable
+import io.reactivex.Single
+import retrofit2.http.GET
+
+interface IndicatorApiService {
+
+    @GET("indicators")
+    fun getIndicators(): Observable<List<HealthIndicator>>
+
+    @GET("")
+    fun getIndicatorById(): Single<HealthIndicator>
+}
